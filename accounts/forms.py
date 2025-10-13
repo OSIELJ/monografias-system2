2d3f17089204ef3a02e45809f567801d2fbc1c6c
RaulRodriguesss <santos.raulrodr@gmail.com>
2025-10-13 17:35:04 -0300

    adição de salvamento de usuário nas respectivas tabelas core

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from allauth.account.forms import SignupForm
 
+from core.models import Aluno, Orientador
+
 class CustomSignupForm(SignupForm):
     TIPO_USUARIO_CHOICES = [
         ("ALUNO", "Aluno"),
@@ -17,4 +19,17 @@ class CustomSignupForm(SignupForm):
         user = super().save(request)
         user.tipo_usuario = self.cleaned_data["tipo_usuario"]
         user.save()
+        if user.tipo_usuario == 'ALUNO':
+            Aluno.objects.create(
+        nome=f"{user.username} ",
+        matricula=f"MATR-{user.id:05d}",
+        email=user.email
+         )
+        elif user.tipo_usuario == 'PROF':
+            Orientador.objects.create(
+                usuario=user,
+                titulacao="Mestre",
+                area_pesquisa="A definir"
+            )
+
         return user
